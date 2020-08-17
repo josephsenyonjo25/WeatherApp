3179823ce327636b612f95ad92996105f42bf4dc
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-17 19:24:14 -0400

    added Header and Footer to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Provider } from './context';
 //import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 //import Header from './components/Header';
 import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Footer from './components/Footer';
 //import CitySearch from './components/CitySearch';
 //import Footer from './components/Footer';
 
@@ -11,7 +13,9 @@ class App extends Component {
     render() {
       return(
         <Provider>
+          <Header />
           <Navigation />
+          <Footer />
         </Provider>
         
       );
