ee96752ad7e6d5cf9e798e0ba1a480e2ca5cfbc9
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-17 19:08:34 -0400

    added Hourly component content

---
+++ src/components/Hourly.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Hourly extends Component {
+    render() { 
+      return (
+        <div className="Hourly"> 
+            <h2>Hourly Forecast</h2>
+          
+        </div>
+      );
+    }
+  }
+
+
+export default Hourly;
