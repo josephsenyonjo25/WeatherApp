9a163da641f032f3aaa1d6a6656297ece3008c09
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-17 18:58:48 -0400

    added Today component content

---
+++ src/components/Today.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Today extends Component {
+    render() { 
+      return (
+        <div className="Today"> 
+            <h2>Today's Forecast</h2>
+          
+        </div>
+      );
+    }
+  }
+
+
+export default Today;
