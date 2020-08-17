91414074220ed8f77dff9b286a4eb5e0a2a42ad5
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-17 19:12:18 -0400

    added FiveDay component content

---
+++ src/components/FiveDay.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class FiveDay extends Component {
+    render() { 
+      return (
+        <div className="FiveDay"> 
+            <h2>5-Day Forecast</h2>
+          
+        </div>
+      );
+    }
+  }
+
+
+export default FiveDay;
