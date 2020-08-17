8a314f16e15b17557b2464bdb0982ab91131e580
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-17 18:55:09 -0400

    added Home content

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() { 
+      return (
+        <div className="Home"> 
+          <p>Welcome to the Weather Page</p> 
+          
+        </div>
+      );
+    }
+  }
+
+
+export default Home;
