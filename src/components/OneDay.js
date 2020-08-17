3717282f8dedf436e8b177a377591a568cb4e6a1
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-17 19:16:35 -0400

    added OneDay component content

---
+++ src/components/OneDay.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class OneDay extends Component {
+    render() { 
+      return (
+        <div className="OneDay"> 
+           <p>Day</p>
+          
+        </div>
+      );
+    }
+  }
+
+
+export default OneDay;
