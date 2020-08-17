71836e39a8ea20dc64494a4a22eab69d6277a8e6
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-17 19:20:51 -0400

    added OneDay components to FiveDay component

--- src/components/FiveDay.js
+++ src/components/FiveDay.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import OneDay from './OneDay';
+
 
 class FiveDay extends Component {
     render() { 
       return (
         <div className="FiveDay"> 
             <h2>5-Day Forecast</h2>
-          
+            <div id ="days">
+                <OneDay />
+                <OneDay />
+                <OneDay />
+                <OneDay />
+                <OneDay />
+            </div>
         </div>
       );
     }
