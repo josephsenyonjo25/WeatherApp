75d668c716424d0f7b3cb88bbc9a87c638d32cc4
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-17 18:18:37 -0400

    updated context

--- src/context.js
+++ src/context.js
@@ -2,23 +2,31 @@ import React, { component } from 'react';
 export const Context = React.createContext();
 export class Provider extends Component {
 	state = {
-		data: ['testing', 1, 2, 3]
-
+		data: ['testing', 1, 2, 3],
+		city: ''
 	}
     ComponentDidMount() {
-        fetch('api ...')
+        /*fetch('api ...')
         .then(res => res.json())
-        .then(data => this.setState({data}))
+		.then(data => this.setState({data}))*/
+		this.getData();
     }
         exampleContextMethod =() => alert('this is a method in the context!!')
 
+	getData =()=>{
+		fetch('api ...')
+        .then(res => res.json())
+        .then(data => this.setState({data}))
+	}
+
+	updateCity =(city) =>this.setState({city});
 
 	render() {
 
-	  let { state, exampleContextMethod } =this
+	  let { state, updateCity, getData, exampleContextMethod } =this
 
 		return(
-		  <Context.Provider value ={{state, exampleContext}}>
+		  <Context.Provider value ={{state, updateCity, getData, exampleContext}}>
 			{this.props.children}
 		  </Context.Provider>
 		)
