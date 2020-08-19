b0de8f8d165d47f831b68f5f90701a2547bf5ff8
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-18 23:08:07 -0400

    added content to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,9 @@ class Home extends Component {
     render() { 
       return (
         <div className="Home"> 
-          <p>Welcome to the Weather Page</p> 
+          <p>Enter a city in the textbox above.</p>
+          <p>Click on 'Today' for the current temperature and weather conditions.</p>
+          <p>Click on '5-Day' for the 5 day extended forecast.</p> 
           
         </div>
       );
