e860a53d37372890b719526106ce21aaaf61350a
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-18 22:16:29 -0400

    added correct weekday names to 5-day forecast

--- src/components/OneDay.js
+++ src/components/OneDay.js
@@ -6,17 +6,20 @@ function OneDay(props) {
     let id = Number(props.id)-1;
     let month = '';
     let day = '';
+    let dayName = '';
+    let dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     let monthNames =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     if(data.state.city){
-        console.log(data.state.days[id].date);
     let date = data.state.days[id].date.split('-');
     month = monthNames[Number(date[1]) -1];
     day = date[2];
+    let dateObj = new Date(date[0], date[1]-1, date[2]);
+    dayName = dayNames[dateObj.getDay()]; // indexing into the dayNames array
     }
     //render() { 
       return (
         <div className="OneDay"> 
-           <p className = "day">Day</p>
+           <p className = "day">{dayName}</p>
            <p className = "date">{month} {day}</p>
            <p className = "high">High: {data.state.days[id].hightemp}&deg;</p>
            <p className = "low">Low: {data.state.days[id].lowtemp}&deg;</p>
